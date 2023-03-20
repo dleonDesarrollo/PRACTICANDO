92f22d7a7c8705a797a90f66769f01fc351e7565
Diego-pc <dleon.desarrollo@gmail.com>
2023-03-20 00:00:39 -0300

    Archivo Insert SQL

---
+++ INSERT_PERSONS.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM practicando.persona
+LIMIT 0, 1000
+
+-- Date: 2023-03-19 23:59
+*/
+INSERT INTO `` (`id_persona`,`nombre`) VALUES (1,'Felipe Salazar');
+INSERT INTO `` (`id_persona`,`nombre`) VALUES (2,'Jaime Murillo');
+INSERT INTO `` (`id_persona`,`nombre`) VALUES (3,'Matias Salinas');
+INSERT INTO `` (`id_persona`,`nombre`) VALUES (4,'Ivan Maldonado');
+INSERT INTO `` (`id_persona`,`nombre`) VALUES (5,'Kevin Silva');
